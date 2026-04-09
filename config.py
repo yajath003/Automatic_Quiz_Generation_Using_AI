a9ded82406b944eee2ef321be4deefb23b741a72
yajath003 <168294733+yajath003@users.noreply.github.com>
2026-04-09 17:24:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
