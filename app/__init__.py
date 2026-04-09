c46cb6a64f31b6ba5c9ba0432a1b22806a3e0b00
yajath003 <168294733+yajath003@users.noreply.github.com>
2026-04-09 18:30:45 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_mail import Mail
-from celery import Celery
+# from flask_mail import Mail
+# from celery import Celery
 from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
-mail = Mail()
+# mail = Mail()
 
-def make_celery(app_name):
-    # Return a basic celery instance. 
-    # Config will be loaded in create_app using lowercase keys to avoid format mixing errors.
-    return Celery(app_name)
+# def make_celery(app_name):
+#     # Return a basic celery instance. 
+#     # Config will be loaded in create_app using lowercase keys to avoid format mixing errors.
+#     return Celery(app_name)
 
-celery = make_celery(__name__)
+# celery = make_celery(__name__)
 
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
@@ -28,17 +28,17 @@ def create_app(config_class=Config):
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
-    mail.init_app(app)
+    # mail.init_app(app)
 
     # Transform CELERY_ configuration to lowercase for Celery 5+
     # This prevents "ImproperlyConfigured: Cannot mix new setting names with old setting names"
     # we only update the celery object with transformed keys
-    celery_conf = {
-        key[7:].lower(): value
-        for key, value in app.config.items()
-        if key.startswith('CELERY_')
-    }
-    celery.conf.update(celery_conf)
+    # celery_conf = {
+    #     key[7:].lower(): value
+    #     for key, value in app.config.items()
+    #     if key.startswith('CELERY_')
+    # }
+    # celery.conf.update(celery_conf)
 
     from app.auth import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
