9cd47a3d22c4abb35cdc0f2ad601e8c62c5a4cf4
yajath003 <168294733+yajath003@users.noreply.github.com>
2026-04-09 18:25:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ class Config:
     # -----------------------------
     # Celery Configuration (Celery 5+ compatible)
     # -----------------------------
-    CELERY_BROKER_URL = os.environ.get('REDIS_URL') or 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL') or 'redis://localhost:6379/0'
+    # CELERY_BROKER_URL = os.environ.get('REDIS_URL') or 'redis://localhost:6379/0'
+    # CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL') or 'redis://localhost:6379/0'
 
-    CELERY_TASK_SERIALIZER = "json"
-    CELERY_RESULT_SERIALIZER = "json"
-    CELERY_ACCEPT_CONTENT = ["json"]
-    CELERY_TIMEZONE = "UTC"
-    CELERY_ENABLE_UTC = True
+    # CELERY_TASK_SERIALIZER = "json"
+    # CELERY_RESULT_SERIALIZER = "json"
+    # CELERY_ACCEPT_CONTENT = ["json"]
+    # CELERY_TIMEZONE = "UTC"
+    # CELERY_ENABLE_UTC = True
 
     # -----------------------------
     # Flask-Mail Configuration
